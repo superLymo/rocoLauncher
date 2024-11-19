61f3ec47eadd93b39fc354cbd990a2922fcfb53e
superLymo <194493782@qq.com>
2024-11-19 16:30:04 +0800

    feat:

--- core/packet/sendProxy.cpp
+++ core/packet/sendProxy.cpp
@@ -6,9 +6,34 @@
 
 namespace roco {
 auto detourSend(SOCKET s, char const * buf, int len, int flags) -> int {
-    sendProxy::ref().submit(s, buf, len, flags);
+    static WSAPROTOCOL_INFOW gameSockInfo {};
 
-    return NO_ERROR;
+    if (roco::sendProxy::ref().getSendSocket() == 0 && len == 49) {
+        if (WSADuplicateSocket(s, GetCurrentProcessId(), &gameSockInfo) == NO_ERROR) {
+            SOCKET copiedSock {WSASocket(
+                FROM_PROTOCOL_INFO, FROM_PROTOCOL_INFO, FROM_PROTOCOL_INFO, &gameSockInfo, 0, 0)};
+
+            if (copiedSock != INVALID_SOCKET) {
+                qDebug() << "i got copiedSock : " << copiedSock << '\n';
+
+                roco::sendProxy::ref().setSendSocket(copiedSock);
+            } else {
+                qDebug() << "copied socket created failed!\n";
+                qDebug() << WSAGetLastError() << '\n';
+            }
+        } else {
+            qDebug() << "socket duplicated failed!\n";
+            qDebug() << WSAGetLastError() << '\n';
+        }
+    }
+
+    if (roco::sendProxy::ref().getSendSocket() == s) {
+        sendProxy::ref().submit(s, buf, len, flags);
+
+        return len;
+    }
+
+    return sendProxy::ref().getSendFunc()(s, buf, len, flags);
 }
 
 auto sendProxy::ref() -> sendProxy & {
