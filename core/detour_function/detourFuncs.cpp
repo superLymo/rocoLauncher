f1e97575cd398c8f9a295000c8a6c27a56ea41ff
superLymo <194493782@qq.com>
2024-11-24 19:23:49 +0800

    refactor: 支持刷新自动重新设置代理套接字

--- core/detour_function/detourFuncs.cpp
+++ core/detour_function/detourFuncs.cpp
@@ -1,4 +1,5 @@
 #include <QDebug>
+#include <QtEndian>
 
 #include "../packet/sendProxy.h"
 
@@ -6,7 +7,10 @@
 
 namespace roco {
 auto detourSend(SOCKET s, char const * buf, int len, int flags) -> int {
-    if (roco::sendProxy::ref().getSendSocket() == 0 && len == 49) {
+    if (len >= 26 &&
+        *reinterpret_cast<quint32 const *>(buf) == static_cast<quint32>(qToBigEndian(0x7467775F)) &&
+        *reinterpret_cast<quint32 const *>(buf + 22) == static_cast<quint32>(qToBigEndian(0x7A6F6E65))
+        ) {
         roco::sendProxy::ref().setSendSocket(s);
     }
 
